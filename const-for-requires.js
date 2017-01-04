7fd35ad2918f0c2c5bb680e972a5424e5e13cd9e
Robert Jackson <rjackson@linkedin.com>
2017-01-04 13:50:34 -0500

    Add `const-for-requires.js` codeshift.

---
+++ const-for-requires.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function transformer(file, api) {
+  let j = api.jscodeshift;
+
+  return j(file.source)
+    .find(j.VariableDeclaration)
+    .forEach(path => {
+      let node = path.node;
+
+      if (
+        (
+          node.declarations[0].init &&
+            node.declarations[0].init.type === 'MemberExpression' &&
+            node.declarations[0].init.object.callee &&
+            node.declarations[0].init.object.callee.name === 'require'
+        ) || (
+          node.declarations[0].init &&
+            node.declarations[0].init.type === 'CallExpression' &&
+            node.declarations[0].init.callee &&
+            node.declarations[0].init.callee.name === 'require'
+        )
+      ) {
+        path.node.kind = 'const';
+      }
+    })
+    .toSource();
+};
